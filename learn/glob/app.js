2c3784413e6eb74177a229e78580ec9c456d5ae7
rosendolu <rosendo2018@163.com>
2020-04-13 17:54:10 +0800

    init

---
+++ learn/glob/app.js
@@ -0,0 +1,9 @@
+const glob = require('glob');
+const { resolve } = require('path');
+const jsArr = glob.sync(resolve(__dirname, './', '**/*.js'));
+console.log(jsArr);
+jsArr
+  .filter((item) => item.indexOf('app.js') === -1)
+  .map((item) => {
+    require(`${item}`).run();
+  });
