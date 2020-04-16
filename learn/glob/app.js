8429879eccb5698fb36234d9dd22898293d0c782
rosendolu <rosendo2018@163.com>
2020-04-16 18:33:09 +0800

    chore:

--- learn/glob/app.js
+++ learn/glob/app.js
@@ -3,7 +3,7 @@ const { resolve } = require('path');
 const jsArr = glob.sync(resolve(__dirname, './', '**/*.js'));
 console.log(jsArr);
 jsArr
-  .filter((item) => item.indexOf('app.js') === -1)
-  .map((item) => {
-    require(`${item}`).run();
-  });
+	.filter((item) => item.indexOf('app.js') === -1)
+	.map((item) => {
+		require(`${item}`).run();
+	});
